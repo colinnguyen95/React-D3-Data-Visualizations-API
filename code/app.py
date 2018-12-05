bff72198776070fa2a49f59632ae8c541ad88555
colinnguyen95 <colinnguyen@Colins-iMac.local>
2018-12-05 06:06:43 -0800

    created all endpoints

--- code/app.py
+++ code/app.py
@@ -1,9 +1,11 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api, reqparse
 import pandas as pd
+import sqlite3
 
 app = Flask(__name__)
-# api = Api(app)
+
+conn = sqlite3.connect('OS_Employee.db')
 
 df = pd.read_csv("SalesDataOrders.csv", index_col=0)
 headers = list(df.columns.values)
@@ -16,43 +18,69 @@ for head in headers[:16]:
 
 @app.route("/")
 def hello():        
+    return 'Hello'
+
+@app.route("/wat")
+def wat():        
     ordersData = df.loc[df["Order Date"].dt.year == 2014]
     orders = ordersData.head(3).reset_index()
-    #return ordersData.head(3).to_html()
     return orders.to_json(orient='records')
 
-@app.route("/top10")
+@app.route('/top10', methods=['GET'])
 def topprofit():
-    orders_year = df.loc[df["Order Date"].dt.year == 2016] 
-    
-    # orders_month = orders_year.loc[orders_year["Order Date"].dt.month == 2]
-    
-    # prod_prof_col = orders_month[["Product Name", "Profit"]]
+    orders_year = df.loc[df["Order Date"].dt.year == 2014] 
     prod_prof_col = orders_year[["Product Name", "Profit"]]
-    
     prod_prof = prod_prof_col.groupby(by="Product Name").sum().sort_values(by="Profit", ascending=False).reset_index()
-    
-    #top10 = prod_prof.head(10)
+    top10 = prod_prof.head(15)
+    return top10.to_json(orient='records')
 
-    return prod_prof.to_json(orient='records')
+@app.route('/profitByMonth', methods=['GET'])
+def profitByMonth():
+    dataByYear = df.loc[df['Order Date'].dt.year == 2017]
+    dataByYear['Order Date'] = dataByYear['Order Date'].dt.month
+    dataByYear['Sales'] = dataByYear['Sales'].round(decimals=2)
+    sales_month_col = dataByYear[['Order Date', 'Sales']]
+    salesByState = sales_month_col.groupby(['Order Date']).sum().sort_values(by=['Order Date'], ascending=True).reset_index()
+    return salesByState.to_json(orient='records')
     
-    #print(prod_prof.tail(10))
-
 @app.route("/profitpersegment")
 def ProfitPerSegment():
+    df['Profit'] = df['Profit'].round(2)
     seg_pro_col = df[['Segment', 'Profit']]
-    
     segment_profits = seg_pro_col.groupby(["Segment"]).sum().sort_values(by=['Profit']).reset_index()
-
     return segment_profits.to_json(orient='records')
 
-# class Student(Resource):
-#     def get(self, name):
-#         return {'student': name}
+@app.route('/signin', methods=['GET', 'POST'])
+def SignIn():
+    if request.method == 'POST':
+        email = request.get_json().get('email')
+        password = request.get_json().get('password')
+        with sqlite3.connect("OS_Employee.db") as db:
+            cursor = db.cursor()
+        find_Employee = ("SELECT * FROM Employee WHERE Email = ? AND Password = ?")
+        cursor.execute(find_Employee,[(email),(password)])
+        results = cursor.fetchall()
+        
+        if results:
+            return jsonify('success')  
+        else:
+            return jsonify('error logging in'), 400
 
-# api.add_resource(Student, '/student/<string:name>')
-
-#app.run(port=5000)
+@app.route('/register', methods=['GET', 'POST'])
+def Register():
+    if request.method == 'POST':
+        firstName = request.get_json().get('firstName')
+        lastName = request.get_json().get('lastName')
+        employeeID = request.get_json().get('employeeID')
+        email = request.get_json().get('email')
+        password = request.get_json().get('password')
+        
+        insertData = '''INSERT INTO Employee(EmployeeID, FirstName, LastName, Email, Password) VALUES 
+        (?,?,?,?,?)'''
+        with sqlite3.connect("OS_Employee.db") as db:
+            cursor = db.cursor()
+        cursor.execute(insertData,[(employeeID),(firstName),(lastName),(email),(password)])
+        db.commit()
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)

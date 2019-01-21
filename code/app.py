a9261a3435b5556c7bb1599420d403dbda1293d3
colinnguyen95 <colinnguyen@Colins-iMac.local>
2019-01-21 00:39:09 -0800

    using correct backend files now

--- code/app.py
+++ code/app.py
@@ -1,12 +1,27 @@
+"""
+Team Project: Web Application --> Data Dashbord 
+
+Libraries Used: Pandas (data manipulation), SQlite (database), CSV (read data), 
+                Flask (python web framework)
+
+Team Functions: We all used pandas for data manipulation
+    Colin Nguyen: Sign In / Register functions using SQL queries+ frontend 
+    Finn Pardon: Top 5 and Lowest 5 Profitable Products 
+    Antoinette Loya: Profit by Month / Profit by Segment
+    Richard De La Rosa: Sales by State / Helped with and learned from everyone's functions
+
+Note: There might be trouble running these functions without 
+        installing required dependencies of the project. A video explaining how 
+        this app works will be included.
+"""
+
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api, reqparse
 import pandas as pd
-import numpy as np
+#import numpy as np
 import sqlite3
 
 app = Flask(__name__)
-
-#conn = sqlite3.connect('OS_Employee.db')
 
 df = pd.read_csv("SalesDataOrders.csv", index_col=0)
 headers = list(df.columns.values)
@@ -21,24 +36,26 @@ for head in headers[:16]:
 def hello():        
     return 'Hello'
 
-@app.route("/wat")
-def wat():        
-    ordersData = df.loc[df["Order Date"].dt.year == 2014]
-    orders = ordersData.head(3).reset_index()
-    return orders.to_json(orient='records')
-
-@app.route('/top10', methods=['GET'])
+@app.route('/top5', methods=['GET'])
 def topprofit():
-    orders_year = df.loc[df["Order Date"].dt.year == 2014] 
+    orders_year = df.loc[df["Order Date"].dt.year == 2017] 
     prod_prof_col = orders_year[["Product Name", "Profit"]]
     prod_prof = prod_prof_col.groupby(by="Product Name").sum().sort_values(by="Profit", ascending=False).reset_index()
     prod_prof['Profit'] = prod_prof['Profit'].round(2)
-    #prod_prof['Profit'] = np.where( prod_prof['Profit'] < 0, '-$' + prod_prof['Profit'].astype(str).str[1:], '$' + prod_prof['Profit'].astype(str))
-    top10 = prod_prof.head(10)
+    top10 = prod_prof.head(5)
     return top10.to_json(orient='records')
 
-@app.route('/profitByMonth', methods=['GET'])
-def profitByMonth():
+@app.route('/lowest5', methods=['GET'])
+def lowestprofit():
+    orders_year = df.loc[df["Order Date"].dt.year == 2017] 
+    prod_prof_col = orders_year[["Product Name", "Profit"]]
+    prod_prof = prod_prof_col.groupby(by="Product Name").sum().sort_values(by="Profit", ascending=False).reset_index()
+    prod_prof['Profit'] = prod_prof['Profit'].round(2)
+    lowest10 = prod_prof.tail(5)
+    return lowest10.to_json(orient='records')
+
+@app.route('/salesByMonth', methods=['GET'])
+def salesByMonth():
     dataByYear = df.loc[df['Order Date'].dt.year == 2017]
     dataByYear['Order Date'] = dataByYear['Order Date'].dt.month
     dataByYear['Sales'] = dataByYear['Sales'].round(decimals=2)
@@ -46,14 +63,21 @@ def profitByMonth():
     salesByState = sales_month_col.groupby(['Order Date']).sum().sort_values(by=['Order Date'], ascending=True).reset_index()
     return salesByState.to_json(orient='records')
     
-@app.route("/profitpersegment")
-def ProfitPerSegment():
+@app.route("/profitpercategory", methods=['GET'])
+def ProfitPerCategory():
     df['Profit'] = df['Profit'].round(2)
     seg_pro_col = df[['Segment', 'Profit']]
     segment_profits = seg_pro_col.groupby(["Segment"]).sum().sort_values(by=['Profit']).reset_index()
     segment_profits['Profit'] = segment_profits['Profit'].round(2)
-    #segment_profits['Profit'] = np.where( segment_profits['Profit'] < 0, '-$' + segment_profits['Profit'].astype(str).str[1:], '$' + segment_profits['Profit'].astype(str))
     return segment_profits.to_json(orient='records')
+
+@app.route("/salesByState", methods=['GET'])
+def SalesByState():
+    df['Sales'] = df['Sales'].round(decimals=2)
+    state_sal_col = df[['State', 'Sales']]
+    salesByState = state_sal_col.groupby(['State']).sum().sort_values(by=['Sales'], ascending=False)
+    return salesByState.reset_index().to_json(orient='records')
+
 
 @app.route('/signin', methods=['GET', 'POST'])
 def SignIn():
@@ -67,7 +91,10 @@ def SignIn():
         results = cursor.fetchone()
         
         if results:
-            return jsonify('success')  
+            find_EmployeeName = ("SELECT FirstName FROM Employee WHERE Email = ? AND Password = ?")
+            cursor.execute(find_EmployeeName,[(email),(password)])
+            firstName = cursor.fetchone()
+            return jsonify(name=firstName[0]), 200
         else:
             return jsonify('error logging in'), 400
 
